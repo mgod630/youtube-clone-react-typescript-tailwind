6519870f6641880754a4adee6b195e6f7ec61ae8
amirhosseinkdm <mgod630@gmail.com>
2023-10-25 11:30:02 +0330

    add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import classNames from "classnames";
+
+type ButtonProps = {
+    title?: string;
+    className?: string;
+    onClick?: React.MouseEventHandler;
+    children?: React.ReactNode;
+};
+
+const Button = ({ children, title, className, onClick }: ButtonProps) => {
+    return (
+        <button
+            onClick={onClick}
+            className={classNames(
+                "p-2 rounded-full bg-slate-100 hover:bg-slate-200 duration-300 whitespace-pre",
+                className
+            )}
+        >
+            {title} {children}
+        </button>
+    );
+};
+export default Button;
