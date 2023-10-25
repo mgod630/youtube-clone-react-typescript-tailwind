4dc4bcc8ddbaa01e47785ddc3bc1fdebe641abe0
amirhosseinkdm <mgod630@gmail.com>
2023-10-25 15:00:21 +0330

    edit video component

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -12,19 +12,21 @@ type VideoProps = {
 const Video = ({ video }: VideoProps) => {
     return (
         <div className="flex flex-col gap-2">
-            <img className="rounded-lg" src={video.image} />
-            <div className="flex items-center">
+            <img className="rounded-lg cursor-pointer hover:scale-[1.02] hover:rounded duration-300" src={video.image} />
+            <div className="flex items-start">
                 <img
                     src={video.logoImg}
                     alt="channel logo"
-                    className="rounded-full w-10 h-10 mr-2"
+                    className="cursor-pointer rounded-full w-10 h-10 mr-2 mt-1"
                 />
                 <div>
-                    <p className="font-semibold">{video.title}</p>
-                    <p className="text-slate-500 text-sm">
+                    <p className="cursor-pointer font-semibold">
+                        {video.title}
+                    </p>
+                    <p className="cursor-pointer text-slate-500 text-sm">
                         {video.channelName}
                     </p>
-                    <p className="text-xs text-slate-500">
+                    <p className="cursor-pointer text-xs text-slate-500">
                         {video.views} views <span>{video.publishTime}</span>
                     </p>
                 </div>
